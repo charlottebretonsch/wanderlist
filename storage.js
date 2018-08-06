4d5038c4cb429dc3a3f953cd96226b48eab992bb
Nathan Gaberel <nathan@gnab.fr>
2018-08-06 22:38:37 +0100

    Add engine.

---
+++ storage.js
@@ -0,0 +1,35 @@
+function makeWanderlist() {
+  const key = 'wanderlist'
+
+  const readAll = () => JSON.parse(localStorage.getItem(key))
+
+  const get = (page, item) => {
+    const data = readAll()
+
+    if (!data || !data[page] || !data[page][item]) return false
+    return data[page][item]
+  }
+  const set = (page, item, value) => {
+    let data = readAll()
+
+    if (!data) data = {}
+    if (!data[page]) data[page] = {}
+
+    data[page][item] = value
+    localStorage.setItem(key, JSON.stringify(data))
+  }
+  const render = (page) => {
+    document.querySelectorAll('.block.item').forEach(element => {
+      const item = element.dataset.name
+      const checkbox = element.querySelector('input')
+      checkbox.checked = get(page, item)
+    })
+  }
+
+  return {
+    render,
+    set
+  }
+}
+
+window.wanderlist = makeWanderlist()
